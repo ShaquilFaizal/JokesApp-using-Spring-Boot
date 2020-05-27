b566e79650f514dd7a86c24050e6d4b1dabc6056
Shaquil Faizal <shaquil.faizal@gmail.com>
2020-05-27 20:45:06 +0530

    add JokeController, RequestMapping and property mode

---
+++ src/main/java/com/shaquilfaizal/JokesApp/controller/JokeController.java
@@ -0,0 +1,28 @@
+package com.shaquilfaizal.JokesApp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.shaquilfaizal.JokesApp.services.JokesService;
+
+@Controller
+public class JokeController {
+
+	private JokesService jokeService;
+	
+	@Autowired
+	public JokeController(JokesService jokeService) {
+		
+		this.jokeService = jokeService;
+	}
+	
+	@RequestMapping({"/",""})
+	public String showJoke(Model model){
+		model.addAttribute("joke",jokeService.getJoke());
+		return "chucknorris";
+	}
+	
+	
+}
