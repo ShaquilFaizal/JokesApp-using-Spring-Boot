63940744ee598de4e67ed465405103de51d71521
Shaquil Faizal <shaquil.faizal@gmail.com>
2020-06-03 00:07:33 +0530

    add configuration

--- src/main/java/com/shaquilfaizal/JokesApp/services/JokesServiceImpl.java
+++ src/main/java/com/shaquilfaizal/JokesApp/services/JokesServiceImpl.java
@@ -1,6 +1,5 @@
 package com.shaquilfaizal.JokesApp.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
@@ -8,17 +7,16 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 @Service
 public class JokesServiceImpl implements JokesService {
 
-	//@Autowired
+	// @Autowired
 	private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-	public JokesServiceImpl() {
-		this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+	public JokesServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+		this.chuckNorrisQuotes = chuckNorrisQuotes;
 	}
 
-	
 	@Override
 	public String getJoke() {
-		
+
 		return chuckNorrisQuotes.getRandomQuote();
 	}
 
